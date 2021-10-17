ac546d68747f5a21fc37cdb21847fd80cc1d5c91
HarshBarash <barashEU@yandex.ru>
2021-10-17 17:04:25 +0500

    OnBoarding + Bottom - Small Refactoring

--- app/src/main/java/harshbarash/github/siriustwo/MainActivity.kt
+++ app/src/main/java/harshbarash/github/siriustwo/MainActivity.kt
@@ -2,17 +2,40 @@ package harshbarash.github.siriustwo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import harshbarash.github.siriustwo.databinding.ActivityMainBinding
+import harshbarash.github.siriustwo.onboarding.ViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
 
     private  lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.viewpager.adapter = ViewPagerAdapter(supportFragmentManager)
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_container
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Setup the bottom navigation view with navController
+        val navigationView = findViewById<BottomNavigationView>(R.id.bottom_main)
+        navigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id == R.id.viewPagerFragment || destination.id == R.id.splashFragment ) {
+                navigationView.visibility = View.GONE
+            } else {
+                navigationView.visibility = View.VISIBLE
+            }
+        }
     }
+
 }
---
+++ app/src/main/java/harshbarash/github/siriustwo/SplashFragment.kt
@@ -0,0 +1,40 @@
+package harshbarash.github.siriustwo
+
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
+
+class SplashFragment : Fragment() {
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        Handler().postDelayed({
+            if(onBoardingFinished()){
+                findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
+            }else{
+                findNavController().navigate(R.id.action_splashFragment_to_viewPagerFragment)
+            }
+        }, 1300)
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    private fun onBoardingFinished(): Boolean{
+        val sharedPref = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        return sharedPref.getBoolean("Finished", false)
+    }
+
+}
