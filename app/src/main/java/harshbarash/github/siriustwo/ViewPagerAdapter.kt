62231eaf4a8def752cf2eee5ae8ff20fb9cd0d45
HarshBarash <barashEU@yandex.ru>
2021-10-17 12:06:18 +0500

    Initial commit

---
+++ app/src/main/java/harshbarash/github/siriustwo/ViewPagerAdapter.kt
@@ -0,0 +1,24 @@
+package harshbarash.github.siriustwo
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+
+class ViewPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager)  {
+
+    private val fragments = listOf(
+        OnBoarding1Fragment(),
+        OnBoarding2Fragment(),
+        OnBoarding3Fragment()
+
+    )
+
+    override fun getCount(): Int {
+        return fragments.size
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return fragments[position]
+    }
+}
